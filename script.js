519b955447105fea7850948c24763b70e3d21e29
なおちゃん <166379976+naochan83275@users.noreply.github.com>
2024-10-13 18:14:13 +0900

    cookieにpath指定を追加

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function change_theme(theme){
   if(theme == 'system'){
     document.getElementById('html').classList.remove("theme-light", "theme-dark");
-    document.cookie = "theme=system; max-age=43200; SameSite=Lax;"
+    document.cookie = "theme=system; max-age=43200; path=/; SameSite=Lax;";
   }else{
     document.getElementById('html').classList.remove("theme-light", "theme-dark");
     document.getElementById('html').classList.add(theme);
-    document.cookie = "theme=" + theme + "; max-age=43200; SameSite=Lax;"
+    document.cookie = "theme=" + theme + "; max-age=43200; path=/; SameSite=Lax;";
   }
 }
 
